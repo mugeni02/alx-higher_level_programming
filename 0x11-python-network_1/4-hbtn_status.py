931616553ae74548a4daeec7f6353d076e904ecf
mugeni02 <mugenir7@gmail.com>
2024-05-28 09:32:24 +0000

    correction

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
-"""
-Script that fetches https://intranet.hbtn.io/status
-"""
 import requests
 
-if __name__ == '__main__':
-    url = "https://intranet.hbtn.io/status"
-    r = requests.get(url)
-    text = r.text
-    print("Body response:")
-    print("\t- type: {}".format(type(text)))
-    print("\t- content: {}".format(text))
+if __name__ == "__main__":
+    url = 'https://intranet.hbtn.io/status'
+    res = requests.get(url)
+    if res.status_code == 200:
+        print('Body response:')
+        print('\t- type: {}'.format(type(str(res))))
+        print('\t- content: {}'.format(res.text))
